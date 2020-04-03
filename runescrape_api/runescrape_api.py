823f174716cc55ca0cae644b74193344be12eb7c
Clement Liaw <cklliaw@uwaterloo.ca>
2020-04-02 23:16:17 -0400

    Updated SQLALCHEMY_DATABASE_URI var to fetch from env variable first

--- runescrape_api/runescrape_api.py
+++ runescrape_api/runescrape_api.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from .extensions import api
 from .models.item import Item
+import os
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:insecure-password@localhost/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('RUNESCRAPE_DB', 'postgresql://postgres:insecure-password@localhost/postgres')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Register extensions
